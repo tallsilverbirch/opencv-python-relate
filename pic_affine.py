ba5a081a90d3c096529a8fc503a8daef2f084f50
baihua <zhangbaihua_neu@163.com>
2021-10-07 22:38:22 +0800

    version 3

--- pic_affine.py
+++ pic_affine.py
@@ -6,7 +6,7 @@ img = cv2.imread(“图片名称，包含完整路径”)
 #仿射变换矩阵
 M=np.array([[0,0.5,-10],[0.5,0,0]])
 #旋转图片
-#通过affine
+#通过affine,rortation
 img1=cv2.warpAffine(img,M, (750,750))
 #显示图片
 cv2.imshow("图片旋转", img1)
